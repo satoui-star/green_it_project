dac2026be52907643aa218d8e1bb69fc570f76b3
totomaNumber1 <tpgh1451@gmail.com>
2025-12-17 18:06:11 +0100

    Update inventory_loader.py
    Récupérer le ficghier csv contenant l'inventaire de LVMH et attribuer les colonnes à des objets python réutilisables

--- env_roi/inventory_loader.py
+++ env_roi/inventory_loader.py
@@ -1,1 +1,25 @@
+import pandas as pd
+
+#Getting the csv file in the github
+def load_inventory(path="assets/Original_data_ROI_calculation.csv"):
+
+    df = pd.read_csv(path)
+
+    inventory = {}
+    lifespan = {}
+    price = {}
+
+    for _, row in df.iterrows():
+        category = row["Category"]
+        item = row["Item"]
+        value = row["Value"]
+
+        if category == "Inventory":
+            inventory[item] = value
+        elif category == "Lifespan":
+            lifespan[item] = value  # months
+        elif category == "Price":
+            price[item] = value     # euros
+
+    return inventory, lifespan, price
 
